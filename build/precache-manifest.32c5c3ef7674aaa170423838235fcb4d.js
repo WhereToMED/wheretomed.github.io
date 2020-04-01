78262d9f5ecd377376ec5509009e8b4ada6a72af
sunshineeleda <thesubjectivesubject@gmail.com>
2020-04-01 22:23:33 +0100

    old master

---
+++ build/precache-manifest.32c5c3ef7674aaa170423838235fcb4d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cc07ba5c482942d85c2e395b6ad58d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f8916051edb8e6d922f",
+    "url": "/static/css/main.8482d8e8.chunk.css"
+  },
+  {
+    "revision": "5b559dfa686c3f0c7242",
+    "url": "/static/js/2.6501dcf9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6501dcf9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f8916051edb8e6d922f",
+    "url": "/static/js/main.cdf49394.chunk.js"
+  },
+  {
+    "revision": "2b3430260969df28d5bb",
+    "url": "/static/js/runtime-main.ba9065ad.js"
+  },
+  {
+    "revision": "cda304961e930d413fceb4be5f2312be",
+    "url": "/static/media/dashboard_full_3.cda30496.png"
+  },
+  {
+    "revision": "b5aa51a1414af81daa393287aa49f181",
+    "url": "/static/media/logob.b5aa51a1.png"
+  },
+  {
+    "revision": "4aaf92d0ed86d1b1165581081977af0d",
+    "url": "/static/media/open-menu.4aaf92d0.svg"
+  }
+]);
